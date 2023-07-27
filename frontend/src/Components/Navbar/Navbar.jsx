4ddcdbf7870fb1b41c3b9f02dee4eef4b89eab67
Siddhant <siddhantsaha.dps@gmail.com>
2023-07-27 23:39:06 +0530

    Navbar and background image added for landing page

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import img from "../../images/flat-lay-medical-composition.jpg";
+import "../Navbar.css";
 const Navbar = () => {
+
+  const [color,setColor] = useState(false);
+
+  const changeColor=()=>{
+    if(window.scrollY>= 200)
+    {
+      setColor(true);
+    }
+    else{
+      setColor(false)
+    }
+  }
+
   return (
     <div>
-      
+      <div onScroll={changeColor}>
+        <nav className={color?"navbar navbar-expand-lg navbar-dark fixed-top navbar-scrolled":"navbar navbar-expand-lg navbar-dark fixed-top"}>
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <img src={img} className="bg-img" alt="bg" />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,82 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./pages/**/*.{html,js,jsx}",
+    "./components/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      white: "#fff",
+      "indigo-950": "#1e1b4b",
+      "black": "#000",
+      "red":"#FF0000",
+      "amber-600":"#d97706",
+      "lightgreen":"#a0dbba"
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      slackside: ["Slackside One", "cursive"],
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  
+  width: {
+    "50rem": "50rem",
+    "40rem": "40rem",
+    "70%": "70%",
+    "100%": "100%",
+    "28rem": "28rem",
+    "64rem": "64rem",
+    "5rem": "5rem",
+  },
+  backgroundImage: {
+    weather: "url('/src/Images/backgroudimage.webp')",
+  },
+  margin: {
+    "30rem": "30rem",
+    "24rem": "24rem",
+    "1rem": "1rem",
+    "5rem": "5rem",
+    "15rem": "15rem",
+    "10px": "10px",
+    "18rem": "18rem",
+  },
+  height: {
+    "35rem": "35rem",
+    "300vh":"300vh",
+    "5rem": "5rem",
+  },
+},
+
+plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+}
+
